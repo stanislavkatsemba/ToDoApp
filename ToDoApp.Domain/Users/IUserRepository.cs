6f7e0c0901aac4567b4cddc89c5f45a835859acb
Stanislav Katsemba <stanislav.katsemba@gmail.com>
2021-11-19 19:20:18 +0100

    Add sql repositories implementation.

---
+++ ToDoApp.Domain/Users/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace ToDoApp.Domain.Users
+{
+    public interface IUserRepository
+    {
+        Task CreateNew(User user);
+
+        Task<User> FindBy(UserId id);
+    }
+}
